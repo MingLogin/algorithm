6ab4c65749fee824a3cf6db73b6e5b4ee053ab68
“MingLogin” <526926722@qq.com>
2021-07-11 00:06:24 +0800

    add tree

--- src/main/java/com/mtc/tree/TreeTest.java
+++ src/main/java/com/mtc/tree/TreeTest.java
@@ -2,6 +2,7 @@ package com.mtc.tree;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class TreeTest {
 
@@ -14,9 +15,29 @@ public class TreeTest {
         inOrderTraveral(node);
         System.out.println("=========后序遍历");
         postOrderTraveral(node);
+        System.out.println("=========非递归    前序遍历");
+        //preOrderTraveralWithStack(node);
         System.out.println(node);
     }
 
+    //非递归前序遍历
+//    private static void preOrderTraveralWithStack(TreeNode root){
+//        Stack<TreeNode> stack = new Stack<>();
+//        TreeNode treeNode = root;
+//        while (treeNode !=null || !stack.isEmpty()){
+//            //访问节点的左节点孩子 并入栈
+//            while (treeNode !=null){
+//                System.out.println(treeNode.data);
+//                stack.push(treeNode.leftChild);
+//                treeNode = treeNode.leftChild;
+//            }
+//            //如果没有左孩子 弹出栈顶元素  并且访问右孩子
+//            if (!stack.isEmpty()){
+//                stack.pop();
+//                treeNode = treeNode.rightChild;
+//            }
+//        }
+//    }
     //前序遍历  根节点   左节点  右节点
     private static void preOrderTraveral(TreeNode treeNode) {
         if (treeNode == null) {
